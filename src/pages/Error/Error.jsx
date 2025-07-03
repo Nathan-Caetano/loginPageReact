0cb1a0c27a26138842d0fdbe2f8ba94c1a7a1821
nathan <nathancaetano71@gmail.com>
2025-07-03 09:24:04 -0300

    Página de erro criada v1

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,6 +1,9 @@
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import "../../App.css";
+import getIcon from "../../icons.jsx";
 import Footer from "../Components/Footer/Footer";
 import Header from "../Components/Header/Header";
+import "./Error.css"
 
 const Error = () => {
   return (
@@ -10,7 +13,12 @@ const Error = () => {
 
       <div className="page-content">
         <main>
-          <h1>Página não encontrada</h1>
+          <div className="container">
+            <h1>404</h1>
+            <h2>Página não encontrada</h2>
+            <p>A página solicitada não foi encontrada</p>
+            <button onClick={() => window.location.href = '/'}>Voltar para a página inicial<i>{getIcon(faArrowRight, '#B8B9C8')}</i></button>
+          </div>
         </main>
       </div>
 
